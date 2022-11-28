75dd77ecb0630cf13060aca691245937b8ce718a
susmita saha <susmita.saha@GRAYMATTER.COM>
2022-11-28 11:18:13 +0530

    Redux project 1st

---
+++ src/redux/contants/action-types.js
@@ -0,0 +1,6 @@
+//ActionTypes is a object
+export const ActionTypes = {
+    SET_PRODUCTS: "SET_PRODUCTS",
+    SELECTED_PRODUCT: "SELECTED_PRODUCT",
+    REMOVE_SELECTED_PRODUCT: "REMOVE_SELECTED_PRODUCT",
+};
